6d0caeca291dcef6b563cbb83840227a5754aeca
=princevora <=princevora522@gmail.com>
2024-07-21 17:52:20 +0530

    http methods error prevention

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -16,5 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (MethodNotAllowedHttpException $e) {
+            return response()->json([
+                'message' => "Method not allowed",
+                'status' => 405
+            ])->setStatusCode(405);
+        });
     })->create();
